f7f2d24185a9ddc6973f63945e700e44e212943b
Paradoksioo <wwesuper202@gmail.com>
2024-06-20 16:27:52 +0300

    Add files via upload

---
+++ assets/js/products.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.product__image').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: false,
+        dots: true,
+        arrows: true,
+        
+    });
+
+    
+});
